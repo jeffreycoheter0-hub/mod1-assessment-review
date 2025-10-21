f2da5774774a2d2f70742ff68b6c46df5a59020c
Jeffrey Cohetero <jeffreycoheter0@gmail.com>
2025-10-21 14:29:15 -0400

    Completed/ read through each .js

--- 1-pure-impure.js
+++ 1-pure-impure.js
@@ -1,9 +1,22 @@
 const nums = [1, 2, 3]
 
 const doublePure = (arr) => {
-  
+    const newArr = arr.map((element) => element * 2);
+    return newArr;
 };
 
 const doubleImpure = (arr) => {
-  
+    // an impure function will modify the original output
+    // create a for loop to iterate the array and double its value
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i] * 2;
+    }
+    // return our modified array
+    return arr;
 };
+
+console.log(doublePure(nums)); // [2, 4, 6]
+console.log(doublePure(nums)); // [2, 4, 6]
+
+console.log(doubleImpure(nums)); // [2, 4, 6]
+console.log(doublePure(nums)); // [4, 8, 12]
--- 3-pass-by-ref.js
+++ 3-pass-by-ref.js
@@ -1,6 +1,6 @@
-const student = require('./2objects-nested-objects')
+const student = require('./2-objects-nested-objects')
 
-const copy = student;
+const copy = student; // not the object but the ref
 copy.name = 'Jordan';
 
 console.log(student.name); 
